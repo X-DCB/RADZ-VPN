55029ae7195921aff372265e5076d907f80415c2
Dexter Cellona Banawon <28706220+X-DCB@users.noreply.github.com>
2019-08-13 20:40:59 +0800

    Add files via upload

---
+++ setup_radz.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+if [ "$(id -u)" -ne 0 ]; then
+	echo -ne "\nPlease execute this script as root.\n"
+	exit 1; fi
+if [ ! -f /etc/debian* ]; then
+	echo -ne "\nFor Debian distro only.\n"
+	exit 1; fi
+export DEBIAN_FRONTEND=noninteractive
+
+radz="https://github.com/X-DCB/RADZ-VPN/raw/master"
+dex="https://github.com/X-DCB/Unix/raw/master"
+bash -c "$(wget -qO- $radz/dropbear.sh)"
+bash -c "$(wget -qO- $radz/fail2ban.sh)"
+bash -c "$(wget -qO- $radz/iptab.sh)"
+bash -c "$(wget -qO- $radz/nginx.sh)"
+bash -c "$(wget -qO- $radz/ovpn-install)"
+bash -c "$(wget -qO- $radz/squid3.sh)"
+bash -c "$(wget -qO- $radz/banner.sh)"
+bash -c "$(wget -qO- $dex/stunnel.sh)"
+bash -c "$(wget -qO- $dex/scripts.sh)"
+
+echo -ne "\nFinshed setting up components.\n
+Script by Dexter Cellona Banawon\n"
+
+exit 0
